22182f659ae094d7d71b74ad90817e9796ef6c10
tikaktus <tikaktus1@gmail.com>
2019-07-30 16:25:59 +0700

    versi 2.3.2

---
+++ database/migrations/2019_07_30_142054_create_schedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('categories_id');
+            $table->integer('rooms_id');
+            $table->date('date_issue');
+            $table->time('start');
+            $table->time('end');
+            $table->integer('employees_id');
+            $table->boolean('status')->default('1');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
